ba72de3071f3c69fd636b789f0d745eb732f370f
dmsvk0217 <dmsvk0217@gmail.com>
2024-01-15 18:25:26 +0900

    react, nodejs setting

---
+++ server/main.js
@@ -0,0 +1,19 @@
+// server.js
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 4000;
+
+// API endpoint 예시
+app.get("/api/message", (req, res) => {
+  res.json({ message: "Hello from the server!" });
+});
+
+// Handles any requests that don't match the ones above
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
