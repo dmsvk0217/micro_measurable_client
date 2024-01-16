82b1aad5cba4ed0594654efebc1d752543536fcf
rladp127 <rladp127@naver.com>
2024-01-16 10:36:01 +0900

    Add Footer Files

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -2,9 +2,9 @@
 
 import React from 'react';
 import './Footer.css'; // 필요에 따라 스타일링을 위한 CSS 파일을 import
-
+ 
 const Footer = () => {
-  return (
+  return ( 
     <footer className="footer-container">
         <p className="CU">Contact Us</p>
         <div className="footer-content">
