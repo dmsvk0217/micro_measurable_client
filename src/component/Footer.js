c92c3aadc64956c8c229dbdc939b1f88793bf186
rladp127 <rladp127@naver.com>
2024-01-16 10:28:40 +0900

    Add footer files and emoj

---
+++ src/component/Footer.js
@@ -0,0 +1,25 @@
+// Footer.js
+
+import React from 'react';
+import './Footer.css'; // 필요에 따라 스타일링을 위한 CSS 파일을 import
+
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+        <p className="CU">Contact Us</p>
+        <div className="footer-content">
+            <div className="mail_content">
+                <img src='img/mail.png' alt="mail" className="mail"/>
+                <p>pohangavengers@gmail.com</p>
+            </div>
+            <div className="mail_content">
+                <img src='img/location.png' alt="location" className="location"/>
+                <p>558, Handong-ro, Heunghae-eup, Buk-gu, Pohang-si, Gyeongsangbuk-do, Republic of Korea</p>
+            </div>
+            <p id="copy">&copy; Pohang Avengers 2024</p>
+        </div>
+    </footer>
+  );
+};
+
+export default Footer;
