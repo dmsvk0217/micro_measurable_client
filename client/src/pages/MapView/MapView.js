4ba0ad0e4430ee13276ae6cc629b7be5ddf179c5
LeeShinwon <leeshin1130@gmail.com>
2024-01-17 00:44:00 +0900

    mapview layout

--- client/src/pages/MapView/MapView.js
+++ client/src/pages/MapView/MapView.js
@@ -3,13 +3,23 @@ import './MapView.css'
 
 function MapView(){
     return (
-        <div className='image-container'>
+        <div className='main-container'>
             <img src='img/map.png' alt="map" className="map"/>
             <div className='building-image-container'>
                 <img src='img/student_building.png' alt='student_building' className='student_building'/>
                 <img src='img/grace_building.png' alt='grace_building' className='grace_building'/>
             </div>
+
+            <div className='option-container'>
+
+            </div>
+
+            <div className='info-container'>
+                <div className='node-info'></div>
+                <div className='legend-info'></div>
+            </div>
         </div>
+
     );
 }
 
