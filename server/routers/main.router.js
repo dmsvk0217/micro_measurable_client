335551f1cf8a0c7b902f54436772dc64386bb533
dmsvk0217 <dmsvk0217@gmail.com>
2024-01-18 13:40:20 +0900

    [server] 6개 api router 구현

---
+++ server/routers/main.router.js
@@ -0,0 +1,36 @@
+export default (app) => {
+  const router = require("express").Router();
+  const mainController = require("../controllers/main.controller");
+
+  router.post(
+    "/all-nodes/all-substances/daily-averages",
+    mainController.allNodesAllSubstancesDailyAverages
+  );
+
+  router.post(
+    "/all-nodes/all-substances/hourly-averages",
+    mainController.allNodesAllSubstancesHourlyAverages
+  );
+
+  router.post(
+    "/node/all-substances/daily-averages",
+    mainController.NodeAllSubstancesDailyAverages
+  );
+
+  router.post(
+    "/node/all-substances/hourly-averages",
+    mainController.nodeAllSubstancesHourlyAverages
+  );
+
+  router.post(
+    "/nodes/substance/monthly-averages",
+    mainController.nodesSubstanceMonthlyAverages
+  );
+
+  router.post(
+    "/all-nodes/substance/monthly-averages",
+    mainController.allNodesSubstanceMonthlyAverages
+  );
+
+  app.use("/api", router);
+};
--- server/server.js
+++ server/server.js
@@ -12,29 +12,25 @@ import calHourlyAverage from "./routines/cal-hourly-data.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const app = express();
-
 cron.schedule("0 * * * *", calHourlyAverage);
 cron.schedule("0 0 * * *", calDailyAverage);
 cron.schedule("0 0 1 * *", calMonthlyAverage);
 
+const app = express();
+
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(
   cors({
     origin: ["http://localhost:3000"],
   })
 );
-app.use(bodyParser.json());
 
-// API endpoint 예시
-app.get("/api/message", (req, res) => {
-  res.json({ message: "Hello from the server!" });
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./index.html"));
 });
 
-// Handles any requests that don't match the ones above
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+require("./routes/user.routes")(app);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
