6eef24856b4167293939b8a639a48281c72a2836
dmsvk0217 <dmsvk0217@gmail.com>
2024-01-19 13:15:03 +0900

    [server] cal-hourly-data.js, addHourlyRawData 수정

--- server/routines/cal-hourly-data.js
+++ server/routines/cal-hourly-data.js
@@ -27,20 +27,9 @@ module.exports = function calHourlyAverage() {
   return;
 };
 
-calHourlyAverage();
-
-function calHourlyAverage() {
-  const { hhmmss } = getDate();
-  console.log(`[${hhmmss}] calHourlyAverage `);
-
-  for (let i = 0; i < NUMBEROFNODE; i++) {
-    calHourlyAverageWithNodeAndHour(i);
-  }
-  return;
-}
-
 async function calHourlyAverageWithNodeAndHour(i) {
   const { yyyyMM, dayDD, hhmmss, hh } = getDate();
+  const hour = (parseInt(hh, 10) + 1).toString();
   let avgValue;
   let dataObject = {
     "node-address": i + 1,
@@ -48,63 +37,49 @@ async function calHourlyAverageWithNodeAndHour(i) {
     timestamp: hhmmss,
   };
 
-  const hourlyRawDataRef = collection(
+  const hourlyNodeRawDataRef = collection(
     db,
-    `hourly-raw-data/${yyyyMM}/day${dayDD}/hour13/node${i + 1}`
+    `hourly-raw-data/${yyyyMM}/day${dayDD}/hour${hour}/node${i + 1}`
   );
-  const nodeHourlyAveragedocRef = doc(
+  const NodehourlyRawDataRef = collection(
     db,
-    `hourly-data/${yyyyMM}/day${dayDD}`,
-    `node${i + 1}`
+    `hourly-raw-data/${yyyyMM}/day${dayDD}/node${i + 1}/hour${hour}`
   );
-  const nodeHourlyAverageCollectionRef = collection(
+  const hourlyNodeDataRef = collection(
     db,
-    `hourly-data/${yyyyMM}/day${dayDD}`
+    `hourly-data/${yyyyMM}/day${dayDD}/hour${hour}/node${i + 1}`
+  );
+  const nodeHourlyDataRef = collection(
+    db,
+    `hourly-data/${yyyyMM}/day${dayDD}/node${i + 1}/hour${hour}`
   );
 
-  try {
-    const querySnapshot = await getDocs(query(hourlyRawDataRef));
+  const querySnapshot = await getDocs(query(hourlyNodeRawDataRef));
 
-    if (querySnapshot.docs.length === 0) {
-      console.log("🚀 ~ calHourlyAverageWithNodeAndHour docs.length = 0");
-      return;
-    }
-
-    // 특정시간 특정노드에 대해서, 모든 물질의 평균값 계산하여 dataObject에 추가
-    for (let j = 0; j < NUMBEROFSUBSTANCE; j++) {
-      const valueArray = querySnapshot.docs.map(
-        (doc) => doc.data()[substanceType[j]]
-      );
-
-      avgValue =
-        valueArray.reduce((acc, value) => acc + value, 0) / valueArray.length;
-
-      dataObject[substanceHourlyAverageType[j]] = avgValue;
-    }
-
-    const docSnapshot = await getDoc(nodeHourlyAveragedocRef);
-
-    if (docSnapshot.exists()) {
-      const data = docSnapshot.data();
-      data[`hour01`] = dataObject;
-      await updateDoc(
-        doc(nodeHourlyAverageCollectionRef, `node${i + 1}`),
-        data
-      );
-
-      console.log("Document updated successfully!");
-    } else {
-      console.log("No such document!");
-      await setDoc(
-        doc(nodeHourlyAverageCollectionRef, `node${i + 1}`),
-        dataObject
-      );
-      console.log("Document updated successfully!");
-    }
-    // await setDoc(doc(hourlyAverageRef, `hour${hh}`), dataObject);
-    console.log("done");
-  } catch (error) {
-    console.log("🚀 ~ calDayAverageWithNodeSubstance ~ error:", error);
+  if (querySnapshot.docs.length === 0) {
+    console.log("🚀 ~ calHourlyAverageWithNodeAndHour docs.length = 0");
+    return;
   }
+
+  // 특정시간 특정노드에 대해서, 모든 물질의 평균값 계산하여 dataObject에 추가
+  for (let j = 0; j < NUMBEROFSUBSTANCE; j++) {
+    const valueArray = querySnapshot.docs.map(
+      (doc) => doc.data()[substanceType[j]]
+    );
+    avgValue =
+      valueArray.reduce((acc, value) => acc + value, 0) / valueArray.length;
+    dataObject[substanceHourlyAverageType[j]] = avgValue;
+  }
+
+  await setDoc(
+    doc(hourlyNodeDataRef, `node${i + 1} : ${yyyyMM}-${dayDD} ${hhmmss}`),
+    dataObject
+  );
+  console.log(`🚀 ~ hourlyNodeDataRef ~ node${i + 1}:`);
+  await setDoc(
+    doc(nodeHourlyDataRef, `node${i + 1} : ${yyyyMM}-${dayDD} ${hhmmss}`),
+    dataObject
+  );
+  console.log(`🚀 ~ nodeHourlyDataRef ~ node${i + 1}:`);
   return;
 }
--- server/routines/test.js
+++ server/routines/test.js
@@ -1,6 +1,7 @@
-const calAllNodeDailyAverage = require("./cal-all-node-daily-data.js");
+// const calAllNodeDailyAverage = require("./cal-all-node-daily-data.js");
 // const calNodeDailyAverage = require("./cal-node-daily-data.js");
 // const calMonthlyAverage = require("./cal-monthly-data.js");
-// const calHourlyAverage = require("./cal-hourly-data.js");
+const calHourlyAverage = require("./cal-hourly-data.js");
 
-calAllNodeDailyAverage();
+// calNodeDailyAverage();
+calHourlyAverage();
