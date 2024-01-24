3c90430807d56921398ddb63a9127e5a14e33db2
dmsvk0217 <dmsvk0217@gmail.com>
2024-01-24 21:04:47 +0900

    [client] RTTable, RTSelection 컴포넌트 분리

--- client/src/pages/RealTimeView/RealTimeView.js
+++ client/src/pages/RealTimeView/RealTimeView.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./RealTimeView.css";
 import RTTable from "./RTTable/RTTable";
 import RTGraph from "./RTGraph/RTGraph";
+import RTSelection from "./RTSelection/RTSelection";
 
 function RealTimeView() {
   return (
     <div className="RT-container">
       <p className="RT-title">실시간 정보 보기</p>
       <div className="RT-content-container">
+        <RTSelection />
         <RTTable />
         <hr className="RT-hr"></hr>
         <RTGraph />
--- client/src/pages/StatisticsView/Day/StatisticsDayView.js
+++ client/src/pages/StatisticsView/Day/StatisticsDayView.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import './StatisticsDayView.css'
-import SDTable from './SDTable/SDTable';
-import SDGraph from './SDGraph/SDGraph';
-import SDSelection from './SDSelection/SDSelection';
+import React from "react";
+import "./StatisticsDayView.css";
+import SDTable from "./SDTable/SDTable";
+import SDGraph from "./SDGraph/SDGraph";
+import SDSelection from "./SDSelection/SDSelection";
 
-function StatisticsDayView(){
-    return (
-        <div className='SD-container'>
-            <p className='SD-title'>통계 보기 &gt; 일별</p>
-            <div className='SD-content-container'>
-                <SDSelection/>
-                <SDTable/>
-                <hr className='SD-hr'></hr>
-                <SDGraph/>
-            </div>
-        </div>
-    );
+function StatisticsDayView() {
+  return (
+    <div className="SD-container">
+      <p className="SD-title">통계 보기 &gt; 일별</p>
+      <div className="SD-content-container">
+        <SDSelection />
+        <SDTable />
+        <hr className="SD-hr"></hr>
+        <SDGraph />
+      </div>
+    </div>
+  );
 }
 
 export default StatisticsDayView;
