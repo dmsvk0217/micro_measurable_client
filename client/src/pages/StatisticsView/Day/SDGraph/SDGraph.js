654d3409351706d749964597bbcd22d1111e369e
LeeShinwon <leeshin1130@gmail.com>
2024-01-24 16:14:55 +0900

    0124신원:SDGraph 일부 완성

--- client/src/pages/StatisticsView/Day/SDGraph/SDGraph.js
+++ client/src/pages/StatisticsView/Day/SDGraph/SDGraph.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Line } from 'react-chartjs-2';
 import 'chart.js/auto';
-// import './SDGraph.css';
+import './SDGraph.css';
+import { chartData,chartOptions } from "./SDChartConfig";
 
 import { Chart, registerables } from 'chart.js';
 import annotationPlugin from 'chartjs-plugin-annotation';
@@ -9,11 +10,13 @@ Chart.register(...registerables, annotationPlugin);
 
 
 function SDGraph(){
+    const data = chartData;
+
     return (
         <div className='RT-graph'>
             <p className='RT-graph-title'>| 그래프 보기 |</p>
             
-            {/* <Line data={data} options={options} /> */}
+            <Line data={data} options={chartOptions} />
         </div>
     );
 }
