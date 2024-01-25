dafbaf4b023f8d60a4f6aa576251d9e69bf0f6d8
dmsvk0217 <dmsvk0217@gmail.com>
2024-01-25 23:18:41 +0900

    temp

--- client/src/pages/StatisticsView/Day/StatisticsDayView.js
+++ client/src/pages/StatisticsView/Day/StatisticsDayView.js
@@ -3,11 +3,10 @@ import "./StatisticsDayView.css";
 import CustomGraph from "../../../components/CustomGraph/CustomGraph";
 import CustomTable from "../../../components/CustomTable/CustomTable";
 import SDSelection from "./SDSelection/SDSelection";
-import { chartData,chartOptions } from "./SDGraphConfig"
+import { chartData, chartOptions } from "./SDGraphConfig";
 import { data, columns } from "./SDTableConfig";
 
 function StatisticsDayView() {
-
   return (
     <div className="SD-container">
       <p className="SD-title">통계 보기 &gt; 일별</p>
