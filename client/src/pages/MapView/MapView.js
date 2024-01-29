9a43f83eab036afb9ffd3d4bf48da16c5747145b
rlap127 <rladp127@naver.com>
2024-01-30 01:51:52 +0900

    Google map 스타일 변경, 지도에맞게 표시 삭제

--- client/src/pages/MapView/MapView.js
+++ client/src/pages/MapView/MapView.js
@@ -57,7 +57,7 @@ function MapView() {
     <div className="main-container">
       <div className="map-container">
         <Wrapper apiKey={"AIzaSyCjp5Sxe-c5mUn1GtfLqEatR0mt7cXYdIM"}>
-          <GoogleMap option={option} options={{ disableDefaultUI: true }} />
+          <GoogleMap option={option} options={{ disableDefaultUI: true, zoomControl: false, }} />
         </Wrapper>
       </div>
       <div className="option-and-info-flex-container">
