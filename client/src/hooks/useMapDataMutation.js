f9aa3d487dbc5a5d60f06ee85d603c8bfa069b50
LeeShinwon <leeshin1130@gmail.com>
2024-01-31 14:27:44 +0900

    0131신원: rt useEffect 중복 수정

--- client/src/hooks/useMapDataMutation.js
+++ client/src/hooks/useMapDataMutation.js
@@ -2,23 +2,54 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { fetchMapData } from '../api/MapApi';
 import useMapStore from '../store/MapStore';
 
-export const useRTTableDataMutation = () => {
+
+export const useMapDataMutation = () => {
   // const queryClient = useQueryClient();
   const { mapLocation, setMapData } = useMapStore();
 
-  const tableMutate = () => {
+  const mapMutate = () => {
     return fetchMapData(mapLocation);
   };
 
+  const makeFormattedSubstance = (responseJson) => {
+    const transformedArray = [];
+    const responseJsonData = responseJson.data;
+
+    console.log(responseJsonData);
+
+    // 데이터 구조를 순회하면서 변환
+    // for( const [key,value] of Object.entries(responseJsonData["day"+day])){
+    //     if (!key.startsWith("node")) continue;
+        
+    //     if(!location.match("전체"))
+    //     if(!location.match(locationFromNodeNumberOptions[parseInt(key.replace("node",""),10)])) continue;
+
+    //     transformedArray.push({
+    //     date: responseJsonData["day"+day]["date"],
+    //     location: locationFromNodeNumberOptions[parseInt(key.replace("node",""),10)],
+    //     pm25: String(value["pm25-daily-average"].toFixed(2)),
+    //     pm10: String(value["pm10-daily-average"].toFixed(2)),
+    //     ch2o: String(value["ch2o-daily-average"].toFixed(2)),
+    //     wind_speed: String(value["wind-speed-daily-average"].toFixed(2)), // 임의의 값으로 설정
+    //     wind_direction: value["wind-direction-daily-average"], // 임의의 값으로 설정
+    //     temperature: `${value["temperature-daily-average"].toFixed(2)} °C`,
+    //     humidity: `${value["humidity-daily-average"].toFixed(2)} %`,
+    //     });
+    // }
+
+    return transformedArray;
+  };
+
   const mutation = useMutation({
-      mutationFn: tableMutate,
+      mutationFn: mapMutate,
       onSuccess: (data, variables, context) => {
         // const queryClient = useQueryClient(); // 캐시 데이터된 무효화 -> 다시 실행 -> 최신 데이터
         setMapData(data);
-        console.log("✅ RTStore table success", data);
+        console.log("✅  MapStore fetch success", data, new Date());
+        makeFormattedSubstance(data);
       },
       onError: (error, variables, context) => {
-        console.log("🚨 RTStore table error", error);
+        console.log("🚨 MapStore fetch error", error);
       },
       onSettled: (data, error, variables, context) => {
         // console.log("🚀 Loading table ...");
--- client/src/pages/MapView/MapView.js
+++ client/src/pages/MapView/MapView.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./MapView.css";
 import GoogleMap from "../../components/GoogleMap/GoogleMap";
 import { Wrapper } from "@googlemaps/react-wrapper";
@@ -6,6 +6,7 @@ import OptionContainer from "./OptionContainer/OptionContainer";
 import LegendInfo from "./LegendInfo/LegendInfo";
 import NodeInfo from "./NodeInfo/NodeInfo";
 import NodeInfoResponsive from "./NodeInfoResponsive/NodeInfoResponsive";
+import { useMapDataMutation } from "../../hooks/useMapDataMutation";
 
 function MapView() {
   const [selectedNode, setSelectedNode] = useState("전체");
@@ -53,6 +54,31 @@ function MapView() {
 
     setOption(newOption);
   };
+
+  const { mutate: mapMutate } = useMapDataMutation();
+
+
+  useEffect(() => {
+    // 초기 데이터 로드
+    mapMutate();
+
+    // 다음 정각까지의 시간(밀리초) 계산
+    const now = new Date();
+    const delay = 3600000 - (now.getMinutes() * 60000 + now.getSeconds() * 1000 + now.getMilliseconds());
+
+    // 정각에 첫 실행을 위한 타이머 설정
+    const timeoutId = setTimeout(() => {
+      mapMutate(); // 데이터 로드
+      // 이후 매 시간마다 데이터 로드
+      setInterval(mapMutate, 3600000);
+    }, delay);
+
+    // 컴포넌트 언마운트 시 타이머 정리
+    return () => {
+      clearTimeout(timeoutId);
+    };
+
+  },[]);
 
   return (
     <div className="main-container">
