a6755018f8844399b81f87d1c2b09d027f60a38e
rladp127 <rladp127@naver.com>
2024-05-01 14:52:22 +0900

    CSS 보완

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,22 +5,25 @@ import { IoMail } from "react-icons/io5";
 
 function Footer(){ 
     return (
-        <footer className="footer-container">
-            <p className="CU">Contact Us</p>
-            <div className="footer-content">
-                <div className="mail_content">
-                <IoMail className="mail"/>
-                    {/* <img src='img/mail.png' alt="mail" className="mail"/> */}
-                    <p>pohangavengers@gmail.com</p>
+        <div>
+            <div className="empty-space"></div>
+            <footer className="footer-container">
+                <p className="CU">Contact Us</p>
+                <div className="footer-content">
+                    <div className="mail_content">
+                    <IoMail className="mail"/>
+                        {/* <img src='img/mail.png' alt="mail" className="mail"/> */}
+                        <p>pohangavengers@gmail.com</p>
+                    </div>
+                    <div className="mail_content">
+                        {/* <img src='img/location.png' alt="location" className="location"/> */}
+                        <FaLocationDot className="location"/>
+                        <p>  558, Handong-ro, Heunghae-eup, Buk-gu, Pohang-si, Gyeongsangbuk-do, Republic of Korea</p>
+                    </div>
+                    <p id="copy">&copy; Pohang Avengers 2024</p>
                 </div>
-                <div className="mail_content">
-                    {/* <img src='img/location.png' alt="location" className="location"/> */}
-                    <FaLocationDot className="location"/>
-                    <p>  558, Handong-ro, Heunghae-eup, Buk-gu, Pohang-si, Gyeongsangbuk-do, Republic of Korea</p>
-                </div>
-                <p id="copy">&copy; Pohang Avengers 2024</p>
-            </div>
-       </footer>
+            </footer>
+        </div>
     );
 }
 
